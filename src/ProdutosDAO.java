feb13035ab45a8a5fbbb794dc47f7b0a618ffae1
maikonrbsx <maikonrbs.x@gmail.com>
2026-06-04 15:30:29 -0300

    Implementando listagem de produtos com busc no banco de dados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -49,7 +49,30 @@ public class ProdutosDAO {
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
-        return listagem;
+        conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos";
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    
+    try {
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+        
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+            listagem.add(produto);
+        }
+        
+        prep.close();
+        conn.close();
+        
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + e.getMessage());
+    }
+    return listagem;
     }
     
     
