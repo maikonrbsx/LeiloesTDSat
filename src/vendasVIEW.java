74f0b9657b5ea124d24afc8a16cf3e7a79841159
maikonrbsx <maikonrbs.x@gmail.com>
2026-06-07 12:41:18 -0300

    Implantando a funcionalidade ao btnVendas e correção de nome da tela de vendasWIEW para vendasVIEW

--- src/vendasVIEW.java
+++ src/vendasVIEW.java
@@ -8,14 +8,14 @@ import javax.swing.table.DefaultTableModel;
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 
-public class vendasWIEW extends javax.swing.JFrame {
+public class vendasVIEW extends javax.swing.JFrame {
     
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(vendasWIEW.class.getName());
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(vendasVIEW.class.getName());
 
     /**
      * Creates new form vendasWIEW
      */
-    public vendasWIEW() {
+    public vendasVIEW() {
         initComponents();
         listarProdutosVendidos();
     }
@@ -131,7 +131,7 @@ public class vendasWIEW extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new vendasWIEW().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> new vendasVIEW().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
